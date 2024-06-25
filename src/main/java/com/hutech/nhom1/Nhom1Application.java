1283618b7419d19749ac20e7269c12e390b0d465
Khang Hoàng <mkhoangvip@gmail.com>
2024-06-25 23:05:50 +0700

    first commit

---
+++ src/main/java/com/hutech/nhom1/Nhom1Application.java
@@ -0,0 +1,13 @@
+package com.hutech.nhom1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nhom1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nhom1Application.class, args);
+	}
+
+}
